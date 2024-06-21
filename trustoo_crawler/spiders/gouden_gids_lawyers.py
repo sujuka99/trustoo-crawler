ad2ed2e2a5aa168a5810a0c5ea593b2b313c6ff3
Ivan Yordanov <hi@ivanyordanov.me>
2024-06-21 14:22:01 +0300

    chore: lint and format

--- trustoo_crawler/spiders/gouden_gids_lawyers.py
+++ trustoo_crawler/spiders/gouden_gids_lawyers.py
@@ -9,8 +9,19 @@ from trustoo_crawler.items import BusinessItem, WorkingTimeItem
 BASE_URL = "https://www.goudengids.nl"
 START_URL = "https://www.goudengids.nl/nl/bedrijven/advocaten/"
 PAGE_URL = "https://www.goudengids.nl/nl/zoeken/advocaten/"
-WEEKDAYS = ("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag")
-XPATH_CONTAINS = "//{element}[contains(concat(' ', normalize-space({attr}), ' '), '{val}')]"
+WEEKDAYS = (
+    "Maandag",
+    "Dinsdag",
+    "Woensdag",
+    "Donderdag",
+    "Vrijdag",
+    "Zaterdag",
+    "Zondag",
+)
+XPATH_CONTAINS = (
+    "//{element}[contains(concat(' ', normalize-space({attr}), ' '), '{val}')]"
+)
+
 
 class WeekDays(StrEnum):
     MONDAY = "Maandag"
@@ -20,6 +31,7 @@ class WeekDays(StrEnum):
     FRIDAY = "Vrijdag"
     SATURDAY = "Zaterdag"
     SUNDAY = "Zondag"
+
 
 class GoudenGidsXPaths(StrEnum):
     NAME = XPATH_CONTAINS.format(element="h1", attr="@itemprop", val="name")
@@ -33,7 +45,9 @@ class GoudenGidsXPaths(StrEnum):
     PAYMENT_OPTIONS = "/html/body/main/div[3]/div/div/div[3]/div/div/div[5]/div"  # TODO
     CERTIFICATES = "/html/body/main/div[3]/div/div/div[3]/div/div/div[4]/div/ul"  # TODO
     OTHER_INFORMATION = "/html/body/main/div[3]/div/div/div[3]/div/div/div[3]/div"  # TODO(Ivan Yordanov): Break down into more useful info
-    WORKING_TIMES = XPATH_CONTAINS.format(element="div", attr="./h3", val="Openingsuren")
+    WORKING_TIMES = XPATH_CONTAINS.format(
+        element="div", attr="./h3", val="Openingsuren"
+    )
     WORKING_DAY = (
         f"{XPATH_CONTAINS.format(element="div", attr="./h3", val="Openingsuren")}"
         f"{XPATH_CONTAINS.format(element="div", attr="div/text()", val="{day}")}"
@@ -49,25 +63,33 @@ class GoudenGidsXPaths(StrEnum):
         f"{XPATH_CONTAINS.format(element="div", attr="@class", val="oh-table__pm whitespace-nowrap")}"
     )
 
-class GoudenGidsLawyersSpider(Spider):
 
+class GoudenGidsLawyersSpider(Spider):
     name = "gouden_gids"
     start_urls = [START_URL]
 
     def parse(self, response: HtmlResponse, **kwargs) -> Iterator[Request]:
         """Find the number of pages for a specific category, call `parse_page` on each."""
-        max_page_xpath = "/html/body/main/div/div/div[2]/div[1]/div[2]/div[2]/ul/li[8]/a/text()"
+        max_page_xpath = (
+            "/html/body/main/div/div/div[2]/div[1]/div[2]/div[2]/ul/li[8]/a/text()"
+        )
         max_page = int(response.xpath(max_page_xpath)[0].get())
         self.log(max_page)
         # for page_number in range(1, max_page + 1):  # TODO(Ivan Yordanov): Uncomment, parametrize
         for page_number in range(1, 2):
             page_url = f"{PAGE_URL}{page_number}/"
             self.log(page_url)
-            yield Request(page_url, callback=self.parse_page, meta={"handle_httpstatus_list": [302]})
+            yield Request(
+                page_url,
+                callback=self.parse_page,
+                meta={"handle_httpstatus_list": [302]},
+            )
 
     def parse_page(self, response: HtmlResponse) -> Iterator[Request]:
         """Find all businesses in a "search" page, call `parse_business_page` on each."""
-        listings_path = "/html/body/main/div/div/div[2]/div[1]/div[2]/div[1]/ol/li/@data-href"
+        listings_path = (
+            "/html/body/main/div/div/div[2]/div[1]/div[2]/div[1]/ol/li/@data-href"
+        )
         listings_urls: list[str] = response.xpath(listings_path).getall()
         # for url in listings_urls:  # TODO(Ivan Yordanov): Uncomment
         for url in listings_urls:
@@ -84,13 +106,15 @@ class GoudenGidsLawyersSpider(Spider):
             phone=self.get_element_text(response, GoudenGidsXPaths.PHONE),
             website=self.get_element_text(response, GoudenGidsXPaths.WEBSITE),
             email=self.get_element_text(response, GoudenGidsXPaths.EMAIL),
-            social_media=self.get_element_texts(response, GoudenGidsXPaths.SOCIAL_MEDIA),
+            social_media=self.get_element_texts(
+                response, GoudenGidsXPaths.SOCIAL_MEDIA
+            ),
             # hourly_rate=self.get_element_text(response, GoudenGidsXPaths.HOURLY_RATE),
             # payment_options=self.get_element_text(response, GoudenGidsXPaths.PAYMENT_OPTIONS),
             # certificates=self.get_element_text(response, GoudenGidsXPaths.CERTIFICATES),
             # other_information=self.get_element_text(response, GoudenGidsXPaths.OTHER_INFORMATION),
             # working_time=self.get_element_text(response, GoudenGidsXPaths.WORKING_TIMES),
-            working_time = self.get_working_times(response),
+            working_time=self.get_working_times(response),
         )
         yield business_item
 
@@ -101,7 +125,12 @@ class GoudenGidsLawyersSpider(Spider):
         return response.xpath(xpath).getall() or []
 
     def get_working_time_day(self, response: HtmlResponse, day: WeekDays) -> str:
-        return response.xpath(f"normalize-space({GoudenGidsXPaths.WORKING_DAY.format(day=day)})").get() or ""
+        return (
+            response.xpath(
+                f"normalize-space({GoudenGidsXPaths.WORKING_DAY.format(day=day)})"
+            ).get()
+            or ""
+        )
 
     def get_working_times(self, response: HtmlResponse) -> WorkingTimeItem:
         return WorkingTimeItem(
