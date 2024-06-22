fcca98cd1ed941059599b44924571b084bee0d18
Ivan Yordanov <hi@ivanyordanov.me>
2024-06-22 10:02:49 +0300

    feat: Collect "other information" in a structured way

--- trustoo_crawler/spiders/gouden_gids_lawyers.py
+++ trustoo_crawler/spiders/gouden_gids_lawyers.py
@@ -1,5 +1,6 @@
 from collections.abc import Iterator
 from enum import StrEnum
+from typing import Any
 
 from scrapy import Request, Spider
 from scrapy.http import HtmlResponse
@@ -21,6 +22,7 @@ WEEKDAYS = (
 XPATH_CONTAINS = (
     "//{element}[contains(concat(' ', normalize-space({attr}), ' '), '{val}')]"
 )
+# Becker's method, credit: https://stackoverflow.com/a/971665/11610149
 XPATH_IF = (
     "concat("
     "   substring({true}, 1, number({condition}) * string-length({true}))"
@@ -47,7 +49,6 @@ class GoudenGidsXPaths(StrEnum):
     WEBSITE = f"{XPATH_CONTAINS.format(element="div", attr="@data-ta", val="WebsiteActionClick")}/@data-js-value"
     EMAIL = f"{XPATH_CONTAINS.format(element="div", attr="@data-ta", val="EmailActionClick")}/@data-js-value"
     SOCIAL_MEDIA = f"{XPATH_CONTAINS.format(element="div", attr="@class", val="flex flex-wrap social-media-wrap")}/a/@href"
-    HOURLY_RATE = "/html/body/main/div[3]/div/div/div[3]/div/div/div[3]/div/div[13]/ul/li/span"  # TODO
     PAYMENT_OPTIONS = (
         f"{XPATH_CONTAINS.format(element="div", attr="h3", val="Betaalmogelijkheden")}"
         f"//li/@title"
@@ -56,24 +57,29 @@ class GoudenGidsXPaths(StrEnum):
         f"{XPATH_CONTAINS.format(element="div", attr="h3", val="Certificeringen")}"
         "//li/span/text()"
     )
-    OTHER_INFORMATION = "/html/body/main/div[3]/div/div/div[3]/div/div/div[3]/div"  # TODO(Ivan Yordanov): Break down into more useful info
-    WORKING_TIMES = XPATH_CONTAINS.format(
-        element="div", attr="./h3", val="Openingsuren"
+    OTHER_INFORMATION_SECTION = (
+        f"{XPATH_CONTAINS.format(element="div", attr="h3", val="Overige informatie")}"
+        f"{XPATH_CONTAINS.format(element="div", attr="span/@class", val="tab__subtitle")}"
     )
+    OTHER_INFORMATION_SECTION_TITLE = "normalize-space(span)"
+    OTHER_INFORMATION_SECTION_VALUE = "//li/span/text()"
+    WORKING_TIMES = XPATH_CONTAINS.format(element="div", attr="h3", val="Openingsuren")
     WORKING_DAY = (
-        f"{XPATH_CONTAINS.format(element="div", attr="./h3", val="Openingsuren")}"
+        f"{XPATH_CONTAINS.format(element="div", attr="h3", val="Openingsuren")}"
         f"{XPATH_CONTAINS.format(element="div", attr="div/text()", val="{day}")}"
     )
     WORKING_TIME_AM = (
-        f"{XPATH_CONTAINS.format(element="div", attr="./h3", val="Openingsuren")}"
+        f"{XPATH_CONTAINS.format(element="div", attr="h3", val="Openingsuren")}"
         f"{XPATH_CONTAINS.format(element="div", attr="div/text()", val="{day}")}"
         f"{XPATH_CONTAINS.format(element="div", attr="@class", val="oh-table__am whitespace-nowrap mr-1 md:mr-3")}"
     )
     WORKING_TIME_PM = (
-        f"{XPATH_CONTAINS.format(element="div", attr="./h3", val="Openingsuren")}"
+        f"{XPATH_CONTAINS.format(element="div", attr="h3", val="Openingsuren")}"
         f"{XPATH_CONTAINS.format(element="div", attr="div/text()", val="{day}")}"
         f"{XPATH_CONTAINS.format(element="div", attr="@class", val="oh-table__pm whitespace-nowrap")}"
     )
+    MAX_PAGE = "/html/body/main/div/div/div[2]/div[1]/div[2]/div[2]/ul/li[8]/a/text()"  # TODO(Ivan Yordanov): Move away from absolute address
+    LISTING = f"{XPATH_CONTAINS.format(element="li", attr="@itemtype", val="http://schema.org/LocalBusiness")}/@data-href"
 
 
 class GoudenGidsLawyersSpider(Spider):
@@ -82,9 +88,7 @@ class GoudenGidsLawyersSpider(Spider):
 
     def parse(self, response: HtmlResponse, **kwargs) -> Iterator[Request]:
         """Find the number of pages for a specific category, call `parse_page` on each."""
-        max_page_xpath = (
-            "/html/body/main/div/div/div[2]/div[1]/div[2]/div[2]/ul/li[8]/a/text()"
-        )
+        max_page_xpath = GoudenGidsXPaths.MAX_PAGE
         max_page = int(response.xpath(max_page_xpath)[0].get())
         self.log(max_page)
         # for page_number in range(1, max_page + 1):  # TODO(Ivan Yordanov): Uncomment, parametrize
@@ -99,9 +103,7 @@ class GoudenGidsLawyersSpider(Spider):
 
     def parse_page(self, response: HtmlResponse) -> Iterator[Request]:
         """Find all businesses in a "search" page, call `parse_business_page` on each."""
-        listings_path = (
-            "/html/body/main/div/div/div[2]/div[1]/div[2]/div[1]/ol/li/@data-href"
-        )
+        listings_path = GoudenGidsXPaths.LISTING
         listings_urls: list[str] = response.xpath(listings_path).getall()
         # for url in listings_urls:  # TODO(Ivan Yordanov): Uncomment
         for url in listings_urls:
@@ -126,7 +128,7 @@ class GoudenGidsLawyersSpider(Spider):
                 response, GoudenGidsXPaths.PAYMENT_OPTIONS
             ),
             certificates=self.get_element_text(response, GoudenGidsXPaths.CERTIFICATES),
-            # other_information=self.get_element_text(response, GoudenGidsXPaths.OTHER_INFORMATION),
+            other_information=self.get_other_information(response),
             working_time=self.get_working_times(response),
         )
         yield business_item
@@ -155,3 +157,16 @@ class GoudenGidsLawyersSpider(Spider):
             saturday=self.get_working_time_day(response, WeekDays.SATURDAY),
             sunday=self.get_working_time_day(response, WeekDays.SUNDAY),
         )
+
+    def get_other_information(self, response: HtmlResponse) -> dict[str, Any]:
+        sections = response.xpath(GoudenGidsXPaths.OTHER_INFORMATION_SECTION)
+        return {
+            section.xpath(GoudenGidsXPaths.OTHER_INFORMATION_SECTION_TITLE).get()
+            or "": [
+                el.strip()
+                for el in section.xpath(
+                    GoudenGidsXPaths.OTHER_INFORMATION_SECTION_VALUE
+                ).getall()
+            ]
+            for section in sections
+        }
